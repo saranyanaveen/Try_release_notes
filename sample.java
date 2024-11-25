6e6e540443ed7672fc05b179b68ad31efe3c8e30
unknown <saranaveen1997@gmail.com>
2024-11-25 17:53:45 +0530

    Fix: Button issue successfully solved

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 public class SampleProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, this is a sample code!");
+        System.out.println("Hello, this is a Button function!");
     }
 }
